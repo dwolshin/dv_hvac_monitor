bbe5fe939a4967da3d6ab5b0e376b36770efb776
dwolshin <35857162+dwolshin@users.noreply.github.com>
2021-08-06 14:29:08 -0400

    Update dv_certstore_test.ino
    Both OTA update and MQTT post are working

--- src/dv_certstore_test/dv_certstore_test.ino
+++ src/dv_certstore_test/dv_certstore_test.ino
@@ -38,9 +38,18 @@ BearSSL::CertStore certStore;
 BearSSL::WiFiClientSecure wifiClient;
 BearSSL::X509List *clientCert;
 BearSSL::PrivateKey *clientKey;
-
 PubSubClient pubSubClient(awsEndpoint, 8883, msgReceived, wifiClient); 
 
+/* Set up values for your repository and binary names */
+#define GHOTA_USER "dwolshin"
+#define GHOTA_REPO "dv_hvac_monitor"
+#define GHOTA_CURRENT_TAG "1.0.2"
+#define GHOTA_BIN_FILE "djw-ota-update-test.ino.nodemcu.bin"
+#define GHOTA_ACCEPT_PRERELEASE 0
+#include <ESP_OTA_GitHub.h>
+
+// Initialise Update Code
+ESPOTAGitHub ESPOTAGitHub(&certStore, GHOTA_USER, GHOTA_REPO, GHOTA_CURRENT_TAG, GHOTA_BIN_FILE, GHOTA_ACCEPT_PRERELEASE);
 
 void setup() {
   Serial.begin(115200);
@@ -48,6 +57,13 @@ void setup() {
   Serial.println();
 
   LittleFS.begin();
+   int numCerts = certStore.initCertStore(LittleFS, PSTR("/certs.idx"), PSTR("/certs.ar"));
+  Serial.printf("Number of CA certs read: %d\n", numCerts);
+  if (numCerts == 0) {
+    Serial.printf("No certs found. Did you unzip the connected device package from AWS and and upload the keys/certs to LittleFS directory before running?\n");
+    return; // Can't connect to anything w/o certs!
+  }
+  
   // Connect to WiFi access point.
   Serial.println(); Serial.println();
   Serial.print("Connecting to ");
@@ -65,22 +81,48 @@ void setup() {
 
   setClock(); // Required for X.509 validation
 
-  int numCerts = certStore.initCertStore(LittleFS, PSTR("/certs.idx"), PSTR("/certs.ar"));
-  Serial.printf("Number of CA certs read: %d\n", numCerts);
-  if (numCerts == 0) {
-    Serial.printf("No certs found. Did you unzip the connected device package from AWS and and upload the keys/certs to LittleFS directory before running?\n");
-    return; // Can't connect to anything w/o certs!
-  }
+ 
+/* This is the actual code to check and upgrade */
+    Serial.println("Checking for update...");
+    if (ESPOTAGitHub.checkUpgrade()) {
+    Serial.print("Upgrade found at: ");
+    Serial.println(ESPOTAGitHub.getUpgradeURL());
+    if (ESPOTAGitHub.doUpgrade()) {
+      Serial.println("Upgrade complete."); //This should never be seen as the device should restart on successful upgrade.
+    } else {
+      Serial.print("Unable to upgrade: ");
+      Serial.println(ESPOTAGitHub.getLastError());
+    }
+    } else {
+    Serial.print("Not proceeding to upgrade: ");
+    Serial.println(ESPOTAGitHub.getLastError());
+    }
+    /* End of check and upgrade code */
+ File cert = LittleFS.open("/DJWESP8266.cert.pem", "r");
+  clientCert = new X509List(cert, cert.size());
+  cert.close();
+  
+   File key = LittleFS.open("/DJWESP8266.private.key", "r");
+  clientKey =new BearSSL::PrivateKey(key, key.size());
+  key.close();
+  
 
-  //open AWS cert and private key from littleFS and read them into the cert store
-
+/*
+ //open AWS cert and private key from littleFS 
   File cert = LittleFS.open("/DJWESP8266.cert.pem", "r"); //can be .der file as well
   File key = LittleFS.open("/DJWESP8266.private.key", "r");
-  //read in the client cert and private keys and set them on the client
-  cert.readBytes(clientCertStr, cert.size()); //copy certificate from file to char array
+cert.readBytes(clientCertStr, cert.size()); //copy certificate from file to char array
   key.readBytes(clientKeyStr, key.size()); //same for private key
-  clientCert = new BearSSL::X509List(clientCertStr);
-  clientKey = new  BearSSL::PrivateKey(clientKeyStr);
+   clientCert = new BearSSL::X509List(clientCertStr);
+  clientKey = new  BearSSL::PrivateKey(clientKeyStr);  
+    
+    BearSSL::WiFiClientSecure *bear = new BearSSL::WiFiClientSecure();
+  // Integrate the cert store with this connection
+  bear->setCertStore(&certStore);
+  Serial.printf("Attempting to fetch https://github.com/...\n");
+  fetchURL(bear, "github.com", 443, "/");  
+  delete bear;
+  */
 
   
 }
@@ -90,9 +132,19 @@ unsigned long lastPublish;
 int msgCount;
 
 void loop() {
+
+  
+  //  BearSSL::WiFiClientSecure *bear = new BearSSL::WiFiClientSecure();
+  // Integrate the cert store with this connection
+  //bear->setCertStore(&certStore);
+ 
+  //pubSubClient.setClient(bear);
+  //read in the client cert and private keys and set them on the client
+  
+  
 // set the cert store and keys for this connection  
   wifiClient.setClientRSACert(clientCert,clientKey);
-  wifiClient.setCertStore(&certStore);
+ wifiClient.setCertStore(&certStore);
   
   pubSubCheckConnect();
 
@@ -106,6 +158,7 @@ void loop() {
     Serial.print("Published: "); Serial.println(msg);
     lastPublish = millis();
   }
+  
 
 }
 
@@ -133,7 +186,7 @@ void pubSubCheckConnect() {
 }
 
 void setCurrentTime() {
-  configTime(3 * 3600, 0, "pool.ntp.org", "time.nist.gov");
+  configTime(3 * 3600, 0, "3.pool.ntp.org", "time.nist.gov");
 
   Serial.print("Waiting for NTP time sync: ");
   time_t now = time(nullptr);
@@ -150,7 +203,7 @@ void setCurrentTime() {
 
 // Set time via NTP, as required for x.509 validation
 void setClock() {
-  configTime(3 * 3600, 0, "pool.ntp.org", "time.nist.gov");
+  configTime(3 * 3600, 0, "0.pool.ntp.org", "time.nist.gov");
 
   Serial.print("Waiting for NTP time sync: ");
   time_t now = time(nullptr);
@@ -164,4 +217,48 @@ void setClock() {
   gmtime_r(&now, &timeinfo);
   Serial.print("Current time: ");
   Serial.print(asctime(&timeinfo));
+}
+
+
+// Try and connect using a WiFiClientBearSSL to specified host:port and dump URL
+void fetchURL(BearSSL::WiFiClientSecure *client, const char *host, const uint16_t port, const char *path) {
+  if (!path) {
+    path = "/";
+  }
+
+  Serial.printf("Trying: %s:443...", host);
+  client->connect(host, port);
+  if (!client->connected()) {
+    Serial.printf("*** Can't connect. ***\n-------\n");
+    return;
+  }
+  Serial.printf("Connected!\n-------\n");
+  client->write("GET ");
+  client->write(path);
+  client->write(" HTTP/1.0\r\nHost: ");
+  client->write(host);
+  client->write("\r\nUser-Agent: ESP8266\r\n");
+  client->write("\r\n");
+  uint32_t to = millis() + 5000;
+  if (client->connected()) {
+    do {
+      char tmp[32];
+      memset(tmp, 0, 32);
+      int rlen = client->read((uint8_t*)tmp, sizeof(tmp) - 1);
+      yield();
+      if (rlen < 0) {
+        break;
+      }
+      // Only print out first line up to \r, then abort connection
+      char *nl = strchr(tmp, '\r');
+      if (nl) {
+        *nl = 0;
+        Serial.print(tmp);
+        break;
+      }
+      Serial.print(tmp);
+    } while (millis() < to);
+  }
+  client->stop();
+  Serial.printf("\n-------\n");
 }
