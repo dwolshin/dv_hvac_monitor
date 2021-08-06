6ca0a702cd261e20390bd4c4431af614d976b194
dwolshin <35857162+dwolshin@users.noreply.github.com>
2021-08-06 11:53:49 -0400

    Working cerstore example
    Working cerstore example, both pub and sub to aws IoT work in this version

--- src/dv_certstore_test/dv_certstore_test.ino
+++ src/dv_certstore_test/dv_certstore_test.ino
@@ -1,34 +1,4 @@
-// Demonstrate the CertStore object with WiFiClientBearSSL
-//
-// Before running, you must download the set of certs using
-// the script "certs-from-mozilla.py" (no parameters)
-// and then uploading the generated .AR file to LittleFS or SD.
-//
-// You do not need to generate the ".IDX" file listed below,
-// it is generated automatically when the CertStore object
-// is created and written to SD or LittleFS by the ESP8266.
-//
-// Why would you need a CertStore?
-//
-// If you know the exact server being connected to, or you
-// are generating your own self-signed certificates and aren't
-// allowing connections to HTTPS/TLS servers out of your
-// control, then you do NOT want a CertStore.  Hardcode the
-// self-signing CA or the site's x.509 certificate directly.
-//
-// However, if you don't know what specific sites the system
-// will be required to connect to and verify, a
-// CertStore can allow you to select from among
-// 10s or 100s of CAs against which you can check the
-// target's X.509, without taking any more RAM than a single
-// certificate.  This is the same way that standard browsers
-// and operating systems verify SSL connections.
-//
-// About the chosen certs:
-// The certificates are scraped from the Mozilla.org current
-// list, but please don't take this as an endorsement or a
-// requirement:  it is up to YOU, the USER, to specify the
-// certificate authorities you will use as trust bases.
+//Based on example from:
 //
 // Mar 2018 by Earle F. Philhower, III
 // Released to the public domain
@@ -37,27 +7,146 @@
 #include <CertStoreBearSSL.h>
 #include <time.h>
 #include "LittleFS.h"
+#include <PubSubClient.h>
 
+//Set only ONE user at a time
+#define DJW
+//#define VIC
+#include "secrets.h" //include MUST come after user definition
 
-#define MAX_PEM_SIZE 4096
+#define MAX_PEM_SIZE 4096 // max size of PEM certs
 
 char clientKeyStr[MAX_PEM_SIZE];
 char clientCertStr[MAX_PEM_SIZE];
 
 /************************* WiFi Access Point *********************************/
+//Set credentials in secrets.h or you can override and set them here
+//const char* WLAN_SSID = "Your SSID";
+//const char* WLAN_PASS = "Your Password";
 
-//#define WLAN_SSID       "...your"
-//#define WLAN_PASS       "...your password..."
-// WiFi credentials.
-const char* WLAN_SSID = "DickStorm";
-const char* WLAN_PASS = "!!!3comuse409";
+//setup for MQTT
+void msgReceived(char* topic, byte* payload, unsigned int len);
 
-
+// Find this awsEndpoint in the AWS Console: Manage - Things, choose your thing
+// choose Interact, its the HTTPS Rest endpoint
+const char* awsEndpoint = "an1v1kdoueaep-ats.iot.us-west-2.amazonaws.com";
 
 // A single, global CertStore which can be used by all
 // connections.  Needs to stay live the entire time any of
 // the WiFiClientBearSSLs are present.
 BearSSL::CertStore certStore;
+BearSSL::WiFiClientSecure wifiClient;
+BearSSL::X509List *clientCert;
+BearSSL::PrivateKey *clientKey;
+
+PubSubClient pubSubClient(awsEndpoint, 8883, msgReceived, wifiClient); 
+
+
+void setup() {
+  Serial.begin(115200);
+  Serial.println();
+  Serial.println();
+
+  LittleFS.begin();
+  // Connect to WiFi access point.
+  Serial.println(); Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(WLAN_SSID);
+
+  WiFi.begin(WLAN_SSID, WLAN_PASS);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println();
+
+  Serial.println("WiFi connected");
+  Serial.println("IP address: "); Serial.println(WiFi.localIP());
+
+  setClock(); // Required for X.509 validation
+
+  int numCerts = certStore.initCertStore(LittleFS, PSTR("/certs.idx"), PSTR("/certs.ar"));
+  Serial.printf("Number of CA certs read: %d\n", numCerts);
+  if (numCerts == 0) {
+    Serial.printf("No certs found. Did you unzip the connected device package from AWS and and upload the keys/certs to LittleFS directory before running?\n");
+    return; // Can't connect to anything w/o certs!
+  }
+
+  //open AWS cert and private key from littleFS and read them into the cert store
+
+  File cert = LittleFS.open("/DJWESP8266.cert.pem", "r"); //can be .der file as well
+  File key = LittleFS.open("/DJWESP8266.private.key", "r");
+  //read in the client cert and private keys and set them on the client
+  cert.readBytes(clientCertStr, cert.size()); //copy certificate from file to char array
+  key.readBytes(clientKeyStr, key.size()); //same for private key
+  clientCert = new BearSSL::X509List(clientCertStr);
+  clientKey = new  BearSSL::PrivateKey(clientKeyStr);
+
+  
+}
+
+//global for main loop
+unsigned long lastPublish;
+int msgCount;
+
+void loop() {
+// set the cert store and keys for this connection  
+  wifiClient.setClientRSACert(clientCert,clientKey);
+  wifiClient.setCertStore(&certStore);
+  
+  pubSubCheckConnect();
+
+  //publish every 10 seconds
+  if (millis() - lastPublish > 10000) {
+    String msg = String("Hello from ESP8266: ") + ++msgCount;
+
+    //this is the magic to publish to the feed
+    pubSubClient.publish("outTopic", msg.c_str());
+    
+    Serial.print("Published: "); Serial.println(msg);
+    lastPublish = millis();
+  }
+
+}
+
+
+
+void msgReceived(char* topic, byte* payload, unsigned int length) {
+  Serial.print("Message received on "); Serial.print(topic); Serial.print(": ");
+  for (int i = 0; i < length; i++) {
+    Serial.print((char)payload[i]);
+  }
+  Serial.println();
+}
+
+void pubSubCheckConnect() {
+  if ( ! pubSubClient.connected()) {
+    Serial.print("PubSubClient connecting to: "); Serial.print(awsEndpoint);
+    while ( ! pubSubClient.connected()) {
+      Serial.print(".");
+      pubSubClient.connect("DJWESP8266");
+    }
+    Serial.println(" connected");
+    pubSubClient.subscribe("/");
+  }
+  pubSubClient.loop();
+}
+
+void setCurrentTime() {
+  configTime(3 * 3600, 0, "pool.ntp.org", "time.nist.gov");
+
+  Serial.print("Waiting for NTP time sync: ");
+  time_t now = time(nullptr);
+  while (now < 8 * 3600 * 2) {
+    delay(500);
+    Serial.print(".");
+    now = time(nullptr);
+  }
+  Serial.println("");
+  struct tm timeinfo;
+  gmtime_r(&now, &timeinfo);
+  Serial.print("Current time: "); Serial.print(asctime(&timeinfo));
+}
 
 // Set time via NTP, as required for x.509 validation
 void setClock() {
@@ -76,123 +165,3 @@ void setClock() {
   Serial.print("Current time: ");
   Serial.print(asctime(&timeinfo));
 }
-
-// Try and connect using a WiFiClientBearSSL to specified host:port and dump URL
-void fetchURL(BearSSL::WiFiClientSecure *client, const char *host, const uint16_t port, const char *path) {
-  if (!path) {
-    path = "/";
-  }
-
-  Serial.printf("Trying: %s:443...", host);
-  client->connect(host, port);
-  if (!client->connected()) {
-    Serial.printf("*** Can't connect. ***\n-------\n");
-    return;
-  }
-  Serial.printf("Connected!\n-------\n");
-  client->write("GET ");
-  client->write(path);
-  client->write(" HTTP/1.0\r\nHost: ");
-  client->write(host);
-  client->write("\r\nUser-Agent: ESP8266\r\n");
-  client->write("\r\n");
-  uint32_t to = millis() + 5000;
-  if (client->connected()) {
-    do {
-      char tmp[32];
-      memset(tmp, 0, 32);
-      int rlen = client->read((uint8_t*)tmp, sizeof(tmp) - 1);
-      yield();
-      if (rlen < 0) {
-        break;
-      }
-      // Only print out first line up to \r, then abort connection
-      char *nl = strchr(tmp, '\r');
-      if (nl) {
-        *nl = 0;
-        Serial.print(tmp);
-        break;
-      }
-      Serial.print(tmp);
-    } while (millis() < to);
-  }
-  client->stop();
-  Serial.printf("\n-------\n");
-}
-
-void setup() {
-  Serial.begin(115200);
-  Serial.println();
-  Serial.println();
-
-  LittleFS.begin();
-// Connect to WiFi access point.
-  Serial.println(); Serial.println();
-  Serial.print("Connecting to ");
-  Serial.println(WLAN_SSID);
-
-  WiFi.begin(WLAN_SSID, WLAN_PASS);
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(500);
-    Serial.print(".");
-  }
-  Serial.println();
-
-  Serial.println("WiFi connected");
-  Serial.println("IP address: "); Serial.println(WiFi.localIP());
-
-  setClock(); // Required for X.509 validation
-
-  
-  
-  int numCerts = certStore.initCertStore(LittleFS, PSTR("/certs.idx"), PSTR("/certs.ar"));
-  Serial.printf("Number of CA certs read: %d\n", numCerts);
-  if (numCerts == 0) {
-    Serial.printf("No certs found. Did you run certs-from-mozilla.py and upload the LittleFS directory before running?\n");
-    return; // Can't connect to anything w/o certs!
-  }
-
-  BearSSL::WiFiClientSecure *bear = new BearSSL::WiFiClientSecure();
-  // Integrate the cert store with this connection
-  bear->setCertStore(&certStore);
-  Serial.printf("Attempting to fetch https://github.com/...\n");
-  fetchURL(bear, "github.com", 443, "/");
-
-  
-File cert = LittleFS.open("/DJWESP8266.cert.pem", "r"); //can be .der file as well
-  File key = LittleFS.open("/DJWESP8266.private.key", "r");
-  
-  cert.readBytes(clientCertStr, cert.size()); //copy certificate from file to char array
-  key.readBytes(clientKeyStr, key.size()); //same for private key
-  X509List clientCert(clientCertStr); 
-  PrivateKey clientKey(clientKeyStr);
-  
-  delete bear;
-}
-
-void loop() {
-
-  
-  
-  
-  Serial.printf("\nPlease enter a website address (www.blah.com) to connect to: ");
-  String site;
-  do {
-    site = Serial.readString();
-  } while (site == "");
-  // Strip newline if present
-  site.replace(String("\r"), emptyString);
-  site.replace(String("\n"), emptyString);
-  Serial.printf("https://%s/\n", site.c_str());
-
-  BearSSL::WiFiClientSecure *bear = new BearSSL::WiFiClientSecure();
-  // Integrate the cert store with this connection
-  bear->setCertStore(&certStore);
-    Serial.printf("\nLoading aws cert and private key: ");
-//  loadCertificates(bear);
-  fetchURL(bear, site.c_str(), 443, "/");
-  delete bear;
-}
-
-
- 
